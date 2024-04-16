509da09ad9377854ab583a85badc513e0d39f6c5
KFH <KFH@LAPTOP-TRN-149>
2024-04-16 14:34:11 +0300

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using EmployeePageTask.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePageTask.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        Employee[] employeeData = [new Employee(1,"Fawaz","Alzamel","fawaz@gmail.com","IT Dep." ),
+        new Employee(2,"Msalam","Alzamel","msalam@gmail.com","KOC" )];
+
+        public IActionResult Index()
+        {
+            return View(employeeData);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeePageTask.Models
+{
+    public class Employee
+    {
+
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }   
+
+        public string Department { get; set; }
+
+
+        public Employee(int EmployeeId, string FirstName, string LastName, string Email, string Department) { 
+
+            this.EmployeeId = EmployeeId;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Department = Department;
+        
+        }
+    }
+}
